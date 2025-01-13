9c0956fe4998003bbc0987873e3cd70fa0329a13
krishhackz <krishhackz.in@gmail.com>
2025-01-13 22:58:48 +0530

    Write a program in C++ to find out if a number is divisible by 5 and 7 using an if-else statement

---
+++ C++/Set1/Question3.cpp
@@ -0,0 +1,20 @@
+/*
+Write a program in C++ to find out if a number 
+is divisible by 5 and 7 using an if-else statement.
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout << "Enter a Number: ";
+    cin >> num;
+
+    if(num%5==0&&num%7==0) cout << num << " is Divisible by 5 and 7 both";
+    else if(num%5==0) cout << num << " is Divisible by 5 but not by 7";
+    else if(num%7==0) cout << num << " is Divisible by 7 but not by 5";
+    else cout << num << " is not Divisible by 5 or 7";
+
+    return 0;
+}
