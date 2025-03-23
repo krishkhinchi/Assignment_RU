70355e1a3a1b2a1668e699c308e188c2b4bc470d
krishhackz <krishhackz.in@gmail.com>
2025-03-23 20:47:49 +0530

    Contact

---
+++ AacharyaSir/Students/frontend/src/Components/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Contact.css"
+import Headers from '../Header/Header'
+
+function Contact() {
+  return (
+    <>
+    <div id="MainContact">
+        <div id="HeaderInContact">
+            <Headers/>
+        </div>
+        <h1 id="HeadingInContact">
+            Contact Us
+        </h1>
+    </div>
+    </>
+  )
+}
+
+export default Contact
