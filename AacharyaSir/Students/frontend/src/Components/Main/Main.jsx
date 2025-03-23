8dc52de3d48eaa247c96bb46063df7172ab37d58
krishhackz <krishhackz.in@gmail.com>
2025-03-23 20:47:09 +0530

    Main

---
+++ AacharyaSir/Students/frontend/src/Components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Main.css"
+import Header from '../Header/Header'
+
+
+function Main() {
+  return (
+    <>
+    <div id='MainBody'>
+      <div id='HeaderInMain'>
+        <Header/>
+      </div>
+      <h1 id='HeadingInMain'>Home Page</h1>
+    </div>
+    </>
+  )
+}
+
+export default Main
